ec681569cd2f2f09f54c9bb8c5090432157abcc1
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-07-07 22:45:51 -0300

    Add Game and Battle classes to implement

---
+++ lib/battle.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Battle
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+end
--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Game
+  def initialize(battle)
+    @battle = battle
+  end
 end
