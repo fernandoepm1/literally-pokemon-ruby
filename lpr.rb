302c7fb98f3ea790a443e3d3c9f4d086b8e6287f
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-07-07 22:46:26 -0300

    Create entrypoint to run the game

---
+++ lpr.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+LPR_VERSION = "1.0"
+
+# Require libraries
+load "lib/player.rb"
+load "lib/pokemon.rb"
+load "lib/move.rb"
+load "lib/battle.rb"
+
+# Start
+load "lib/game.rb"
