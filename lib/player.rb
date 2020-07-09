c6d2efb8d3bef0be20d45a0ea7217b10223c73f5
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-07-08 22:07:46 -0300

    Update player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,15 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
-class Player
-  def initialize(name)
-    @name = name
+module LPR
+  class Player
+    attr_accessor :name
+    attr_accessor :type
+
+    enum type: [:human, :cpu]
+
+    def initialize(name)
+      @name = name
+    end
   end
 end
