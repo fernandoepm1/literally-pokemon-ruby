938f5b301e40824dfed28dff4640d8dd9c71dac3
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-07-08 23:07:29 -0300

    Load missing libs in lpr

--- lpr.rb
+++ lpr.rb
@@ -3,10 +3,11 @@
 LPR_VERSION = "1.0"
 
 # Require libraries
+load "lib/battle.rb"
+load "lib/interface.rb"
+load "lib/move.rb"
 load "lib/player.rb"
 load "lib/pokemon.rb"
-load "lib/move.rb"
-load "lib/battle.rb"
 
 # Start
 load "lib/game.rb"
