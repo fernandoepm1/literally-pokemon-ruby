891c9dc9984bf9e6a19bb16f09355a77db8f7888
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-07-09 23:16:15 -0300

    Add ask game mode method to interface

--- lib/interface.rb
+++ lib/interface.rb
@@ -7,6 +7,14 @@ module LPR
       puts title
     end
 
+    def ask_game_mode(question = '')
+      question << "Currently available game modes\n\n"
+      question << "(1) Singleplayer\t"
+      question << "(2) Multiplayer\n"
+      question << "Choose one: "
+      print question
+    end
+
     def ask(question)
     end
   end
