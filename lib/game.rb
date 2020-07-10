4c50529c79c1d6de9c52ecdc43c4d2d131f25458
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-07-09 23:19:45 -0300

    Begin implementation of game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,39 @@
 #!/usr/bin/env ruby
 
-interface = LPR::Interface.new
-interface.draw_title
+class LPR::Game
+  attr_accessor :is_running
+  attr_accessor :mode
 
-running = false
+  attr_reader :interface
 
-while running
-  # run game
+  def initialize
+    @interface = LPR::Interface.new
+  end
+
+  def run
+    @is_running = true
+
+    while @is_running
+      @interface.draw_title
+      set_mode
+
+      @is_running = false
+    end
+  end
+
+  private
+
+  def set_mode
+    while true
+      @interface.ask_game_mode
+      @mode = gets.chomp
+
+      break if valid_mode?
+      puts "Invalid option, try again.\n"
+    end
+  end
+
+  def valid_mode?
+    @mode == '1' || @mode == '2'
+  end
 end
