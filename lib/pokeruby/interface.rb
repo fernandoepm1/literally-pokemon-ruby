da428d741bb4d0f0a640e3dbc347cb7428ee9419
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-07-12 22:20:02 -0300

    Update gamemode selection

--- lib/pokeruby/interface.rb
+++ lib/pokeruby/interface.rb
@@ -8,8 +8,10 @@ class LPR::Interface
 
   def ask_game_mode(question = '')
     question << "Currently available game modes\n\n"
-    question << "(1) Singleplayer\t"
-    question << "(2) Multiplayer\n"
+    question << "(1) Arcade Mode - "
+    question << "Fight waves of pokémons until you faint.\n"
+    question << "(2) Versus Mode - "
+    question << "2 players battling, alternating turns.\n\n"
     question << "Choose one: "
     print question
   end
